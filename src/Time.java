501dacc29e2c14aa85c606fd067179dde06c8db4
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2019-11-22 19:15:23 -0800

    fixed small math issues

--- src/Time.java
+++ src/Time.java
@@ -65,7 +65,7 @@ public class Time {
 	 */
 	private double GST(){
 
-		double d = (jd() -2451545.5);
+		double d = (jd() -2451544.5);
 		double T = d / 36525.0;
 		double temp = 6.697374558+(2400.051336 * T)+(.000025862 * Math.pow(T, 2));
 		temp = temp %24;
